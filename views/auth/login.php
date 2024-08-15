a2a68f24b94dbb974e9fd4d8863c221ada939ee7
Rbgc11 <rbgarciacaparros@gmail.com>
2024-08-15 11:15:56 +0200

    first commit

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,5 +1,5 @@
 <h1 class="nombre-pagina">Login</h1>
-<p class="descripcion-pagina">Inicia Sesión con tus datos</p>
+<p class="descripcion-pagina">Inicia Sesión con tus datos.</p>
 
 <?php 
     include_once __DIR__ . "/../templates/alertas.php";
